11b7eaf715344e28c8c035b32e896d24972433c3
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-04 21:50:16 +0800

    Update home.tsx

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,7 +360,7 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
+        <title>Ailogy Chatbot UI</title>
         <meta name="description" content="ChatGPT but better." />
         <meta
           name="viewport"
