2a2a9d1eba43d375be1ff265b0394a41cfde7475
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-04 22:22:11 +0800

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -381,12 +381,12 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/juncodeworld/chatbot-ui.git"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Ailogy
         </a>
         .{' '}
         {t(
