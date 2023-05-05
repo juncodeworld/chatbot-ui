feab0d78a7ab4590b5042ec733fe6d9fb5836297
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-05 19:18:35 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -405,7 +405,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'GPT AI'
+                      'Hello AI'
                     )}
                   </div>
 
