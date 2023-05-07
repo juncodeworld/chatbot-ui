582c7e474c9273a3f5d01134f73272f844ecca9c
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-07 11:19:15 +0800

    Update ChatMessage.tsx

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -2,7 +2,7 @@ import {
   IconCheck,
   IconCopy,
   IconEdit,
-  Iconactivity-heartbeat,
+  IconActivityHeartbeat,
   IconTrash,
   IconUser,
 } from '@tabler/icons-react';
@@ -136,7 +136,7 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
       <div className="relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] text-right font-bold">
           {message.role === 'assistant' ? (
-            <Iconactivity-heartbeat size={30} />
+            <IconActivityHeartbeat size={30} />
           ) : (
             <IconUser size={30} />
           )}
