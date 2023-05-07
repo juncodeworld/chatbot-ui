5d3ec2738f61f89d7c642651565768984e928106
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-07 11:13:34 +0800

    Update ChatLoader.tsx

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -1,4 +1,4 @@
-import { activity-heartbeat } from '@tabler/icons-react';
+import { iconactivity-heartbeat } from '@tabler/icons-react';
 import { FC } from 'react';
 
 interface Props { }
@@ -11,7 +11,7 @@ export const ChatLoader: FC<Props> = () => {
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] items-end">
-          <activity-heartbeat size={30} />
+          <iconactivity-heartbeat size={30} />
         </div>
         <span className="animate-pulse cursor-default mt-1">▍</span>
       </div>
