6bd1f3f525d825c3c022b23edf815f27559bcdd3
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-12 22:35:10 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -54,7 +54,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <h2 style={{fontSize: '20px'}}>联系站长部署私有帐号</h2>
         <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
         <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有GPT－API无限制</p>
-        <p style={{fontSize: '18px', textAlign: 'center'}}>进入官网购买GPT月卡</p>
+        <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='https://www.aiology.cn/gpt-monthly/'}>进入官网购买GPT月卡</p>
         <button onClick={handleClosePopup}></button>
       </div>
       )}
