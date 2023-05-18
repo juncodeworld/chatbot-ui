6e126483d120daa66beac19d6028250c943810f7
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-18 09:39:49 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,65 +1,72 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
-import { appWithTranslation } from 'next-i18next';
+import { withTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
-import '@/styles/globals.css';
+import React, { useEffect, useState, Fragment } from 'react';
 
-import React, { useEffect, useState } from 'react';
-
+const queryClient = new QueryClient(); // 添加这行
 const inter = Inter({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [showPopup, setShowPopup] = useState(false);
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowPopup(true);
-    }, 2000000); // 20 minutes in milliseconds
+    }, 1200000); // 20 minutes in milliseconds
 
     return () => clearTimeout(timer);
   }, []);
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  const queryClient = new QueryClient();
+  const handleClosePopup = () => {
+    if (password === '666518') { // 替换 correct_password 为实际的正确密码
+      setShowPopup(false);
+    } else {
+      alert('密码错误');
+    }
+  };
+
+  const popupContent = (
+    <div
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'rgba(242, 242, 242, 0.6)',
+        padding: '10px',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+        zIndex: '9999',
+        width: '800px', 
+        height: '220px',
+        textAlign: 'center'
+      }}
+    >
+      <h1 style={{fontSize: '24px'}}> Ailogy.cn</h1>
+      <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
+      <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有GPTAPI无限制</p>
+      <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='https://www.aiology.cn/gpt-monthly/'}>进入官网购买GPT月卡</p>
+      <input type="password" value={password} onChange={handlePasswordChange} />
+      <button onClick={handleClosePopup}>关闭</button>
+    </div>
+  );
 
   return (
-    <div className={inter.className}>
+    <React.Fragment> {/* 替换 Fragment 为 React.Fragment */}
+      {showPopup && popupContent}
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
-      {showPopup && (
-        <div
-        style={{
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          backgroundColor: 'rgba(242, 242, 242, 0.6)',
-          padding: '10px',
-          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
-          zIndex: '9999',
-          width: '800px', 
-          height: '220px',
-          textAlign: 'center'
-        }}
-      >
-        <h1 style={{fontSize: '24px'}}> www.Ailogy.cn</h1>
-        <h2 style={{fontSize: '20px'}}>联系站长部署私有帐号</h2>
-        <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
-        <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有官方API无限制</p>
-        <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='http://ailogy.cn/archives/352'}>进入官网购买体验月卡</p>
-       <button style={{fontSize: '18px', textAlign: 'center'}} onClick={handleClosePopup}>我已购买</button>
-      </div>
-      )}
-    </div>
+    </React.Fragment>
   );
 }
 
-export default appWithTranslation(MyApp);
+export default withTranslation('common')(MyApp); // 添加 withTranslation
