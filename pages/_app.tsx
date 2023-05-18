ba7d352ea8f7eb62f11100cd8b95cbe7d1f6e878
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-18 15:16:31 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
@@ -9,7 +8,15 @@ import '@/styles/globals.css';
 
 import React, { useEffect, useState } from 'react';
 
+
 const inter = Inter({ subsets: ['latin'] });
+
+const handleClosePopup = (event) => {
+  if (event.target.value) {
+    setShowPopup(false);
+  }
+}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [showPopup, setShowPopup] = useState(false);
@@ -17,7 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowPopup(true);
-    }, 2000000); // 20 minutes in milliseconds
+    }, 1200000); // 20 minutes in milliseconds
 
     return () => clearTimeout(timer);
   }, []);
@@ -29,36 +36,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    <div className={inter.className}>
-      <Toaster />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-      {showPopup && (
-        <div
-        style={{
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          backgroundColor: 'rgba(242, 242, 242, 0.6)',
-          padding: '10px',
-          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
-          zIndex: '9999',
-          width: '800px', 
-          height: '220px',
-          textAlign: 'center'
-        }}
-      >
-        <h1 style={{fontSize: '24px'}}> www.Ailogy.cn</h1>
-        <h2 style={{fontSize: '20px'}}>联系站长部署私有帐号</h2>
-        <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
-        <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有官方API无限制</p>
-        <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='http://ailogy.cn/archives/352'}>进入官网购买体验月卡</p>
-       <button style={{fontSize: '18px', textAlign: 'center'}} onClick={handleClosePopup}>我已购买</button>
-      </div>
-      )}
+ <div className="popup">
+  {showPopup && (
+    <div className="popup-inner">
+      <h1 style={{fontSize: '24px'}}> www.Ailogy.cn</h1>
+      <h2 style={{fontSize: '20px'}}></h2>
+      <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
+      <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有官方API无限制</p>
+      <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='http://ailogy.cn/archives/352'}>进入官网购买体验月卡</p>
+      <input type="text" style={{ fontSize: '18px', textAlign: 'center' }} placeholder="输入体验码" onChange={handleClosePopup} />
     </div>
+  )}
+</div>
   );
 }
 
