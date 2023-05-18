f7d37d0f16e34d65919bffef27e451d4c1f6b391
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-05-18 15:30:57 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,17 +8,28 @@ import '@/styles/globals.css';
 
 import React, { useEffect, useState } from 'react';
 
-
 const inter = Inter({ subsets: ['latin'] });
 
-const handleClosePopup = (event: React.ChangeEvent<HTMLInputElement>) => {
-  if (event.target.value) {
-    setShowPopup(false);
-  }
-};
+function Popup({ setShowPopup }: { setShowPopup: (show: boolean) => void }) {
+  const handleClosePopup = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value) {
+      setShowPopup(false);
+    }
+  };
 
-
-
+  return (
+    <div className="popup">
+      <div className="popup-inner">
+        <h1 style={{ fontSize: '24px' }}> www.Ailogy.cn</h1>
+        <h2 style={{ fontSize: '20px' }}></h2>
+        <p style={{ fontSize: '18px', textAlign: 'center' }}>体验帐号：仅用于体验有限制</p>
+        <p style={{ fontSize: '18px', textAlign: 'center' }}>私有帐号：自有官方API无限制</p>
+        <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href = 'http://ailogy.cn/archives/352'}>进入官网购买体验月卡</p>
+        <input type="text" style={{ fontSize: '18px', textAlign: 'center' }} placeholder="输入体验码" onChange={handleClosePopup} />
+      </div>
+    </div>
+  );
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [showPopup, setShowPopup] = useState(false);
@@ -31,25 +42,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     return () => clearTimeout(timer);
   }, []);
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
-
   const queryClient = new QueryClient();
 
   return (
- <div className="popup">
-  {showPopup && (
-    <div className="popup-inner">
-      <h1 style={{fontSize: '24px'}}> www.Ailogy.cn</h1>
-      <h2 style={{fontSize: '20px'}}></h2>
-      <p style={{fontSize: '18px', textAlign: 'center'}}>体验帐号：仅用于体验有限制</p>
-      <p style={{fontSize: '18px', textAlign: 'center'}}>私有帐号：自有官方API无限制</p>
-      <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='http://ailogy.cn/archives/352'}>进入官网购买体验月卡</p>
-      <input type="text" style={{ fontSize: '18px', textAlign: 'center' }} placeholder="输入体验码" onChange={handleClosePopup} />
-    </div>
-  )}
-</div>
+    <>
+      {React.Children.map(Component, (child) => {
+        if (React.isValidElement(child) && child.type.name === 'Popup') {
+          return React.cloneElement(child, { setShowPopup });
+        }
+        return child;
+      })}
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
 
