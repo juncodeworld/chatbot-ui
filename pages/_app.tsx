9f818e430ee48268ab04f8fe3855bf541181b56e
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-06-11 14:08:06 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,25 @@ import { Inter } from 'next/font/google';
 
 import '@/styles/globals.css';
 
+import React, { useEffect, useState } from 'react';
+
 const inter = Inter({ subsets: ['latin'] });
 
-function App({ Component, pageProps }: AppProps<{}>) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 1000000); // 20 minutes in milliseconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   const queryClient = new QueryClient();
 
   return (
@@ -18,8 +34,31 @@ function App({ Component, pageProps }: AppProps<{}>) {
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
+      {showPopup && (
+        <div
+        style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(242, 242, 242, 0.6)',
+          padding: '10px',
+          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+          zIndex: '9999',
+          width: '800px', 
+          height: '220px',
+          textAlign: 'center'
+        }}
+      >
+        <h1 style={{fontSize: '24px'}}> www.Ailogy.cn</h1>
+        <h2 style={{fontSize: '20px'}}>通知：AI助理即将更新！</h2>
+        <p style={{fontSize: '18px', textAlign: 'center'}}>请尽快保存对话相关资料</p>
+        <p style={{ fontSize: '18px', textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => window.location.href='http://ailogy.cn/'}>进入官网</p>
+       <button style={{fontSize: '18px', textAlign: 'center'}} onClick={handleClosePopup}>确定</button>
+      </div>
+      )}
     </div>
   );
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(MyApp);
