b60c4ae9bf7096b94632bd051f718cf927f027b5
juncodeworld <128709630+juncodeworld@users.noreply.github.com>
2023-07-15 16:26:11 +0800

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -8,18 +8,18 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo-16k',
+  GPT_3_5_turbo = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_turbo;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
+  [OpenAIModelID.GPT_3_5_turbo]: {
+    id: OpenAIModelID.GPT_3_5_turbo,
     name: 'gpt-3.5-turbo-16k',
     maxLength: 48000,
     tokenLimit: 16000,
